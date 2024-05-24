135f32ad88eb8ae16854697c9351ad88a762788e
briemodd <bibiairam1@gmail.com>
2024-05-24 11:02:59 -0300

    iniciando o codigo. ja fiz a interface

--- app/src/main/java/com/example/galeria/PhotoActivity.java
+++ app/src/main/java/com/example/galeria/PhotoActivity.java
@@ -4,31 +4,36 @@ import android.os.Bundle;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Toolbar toolbar = findViewById(R.id.tbMain);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar =findViewById(R.id.tbPhoto);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
 
     }
 
-    private void setSupportActionBar(Toolbar toolbar) {
+
+    @Override
+    public void setSupportActionBar(@Nullable androidx.appcompat.widget.Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
     }
-
-
 }
